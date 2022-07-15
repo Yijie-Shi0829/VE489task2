c73d73f21037833ab3d4d192742de3b6e627a96c
yijieshi <yijieshi@umich.edu>
2022-07-15 01:42:08 -0400

    first commit

---
+++ receiver.py
@@ -0,0 +1,140 @@
+import socket
+import struct
+import binascii
+
+
+def parse_pkt(pkt):
+    """
+    Parse the received packet
+    Args:
+        pkt: packet received
+    Returns:
+        seq_num: sequence number
+        crc_num: crc number
+        data: data contained in the packet
+    """
+    seq_num = struct.unpack('=I', pkt[0:4])[0]
+    crc_num = struct.unpack('=H', pkt[4:6])[0]
+    data = pkt[6:].decode('UTF-8', errors='ignore')
+    return seq_num, crc_num, data
+
+
+def check_crc(crc_num, data):
+    """
+    check whether crc is correct
+    Args:
+        crc_num: crc unmber to be checked
+        data: parsed data
+    Returns:
+        bool: whether crc number is correct
+    """
+    return binascii.crc_hqx(bytes(data, 'UTF-8'), 0) == crc_num
+
+
+def seq_in_range(r_next, size_window, max_seq, seq):
+    """
+    check whether sequence number is in right range
+    Args:
+        r_next: next packet to receive
+        size_window: size of receiving window
+        max_seq: maximum of sequence number
+        seq: sequence number of the packet
+
+    Returns:
+        bool: whether sequence number is in right range
+    """
+    if (r_next + size_window - 1) % max_seq > r_next:
+        if seq >= r_next and seq <= seq < (r_next + size_window - 1) % max_seq:
+            return True
+        else:
+            return False
+    elif (r_next + size_window - 1) % max_seq < r_next:
+        if seq < r_next and seq > (r_next + size_window - 1) % max_seq:
+            return False
+        else:
+            return True
+
+
+def prepare_ack_pkt(seq_num, ack_or_not, r_next):
+    """
+    prepare ack packet to respond
+    Args:
+        seq_num: the sequence number that the packet is responding
+        ack_or_not: whether receiver ack the packet
+    Returns:
+        packet: the return packet
+    """
+    return struct.pack('=I', seq_num) + struct.pack('=H', ack_or_not) + struct.pack('=I', r_next)
+
+
+def receiver():
+    r_next = 0  # next sequence number to receive
+    buffer = {}  # buffer to save packets
+    receive_data = []  # concat all data in right order
+    size_window = 8  # size of receiving window
+    max_seq = 16  # maximum of sequence number
+    nack = False  # whether it's nack or not
+    rec_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # here we choose a arbitrary port 10000
+
+    # TODO: IP address
+    rec_socket.bind(("IP address", 10000))
+    rec_socket.listen(1)
+    send_socket, send_addr = socket.accept()
+
+    # main process of the receiver
+    while True:
+        try:
+            send_socket.settimeout(10)
+            send_pkt = send_socket.recv(2048)
+            send_socket.settimeout(None)
+            seq, crc, data = parse_pkt(send_pkt)
+
+            # check crc
+            crc_true = check_crc(crc, data)
+            if crc_true:
+                if seq_in_range(r_next, size_window, max_seq, seq):
+                    if seq == r_next:
+                        receive_data.append(data)
+                        next = (r_next + 1) % max_seq
+                        for i in range(size_window - 1):
+                            num = (r_next + i + 1) % max_seq
+                            if num not in buffer.keys():
+                                break
+                            else:
+                                receive_data.append(buffer[num])
+                                next = (next + 1) % max_seq
+                                del buffer[num]
+                        r_next = next
+                        nack = False
+                        ack_pkt = prepare_ack_pkt(seq, 1, r_next)
+                        send_socket.send(ack_pkt)
+                    else:
+                        if seq not in buffer:
+                            buffer[seq] = data
+                        if not nack:
+                            nack = True
+                            ack_pkt = prepare_ack_pkt(seq, 0, r_next)
+                        else:
+                            ack_pkt = prepare_ack_pkt(seq, 1, r_next)
+                        send_socket.send(ack_pkt)
+                # else:
+                #     # TODO: not in range
+                #     ack_pkt = prepare_ack_pkt(seq, 1, r_next)
+                #     send_socket.send(ack_pkt)
+        except ConnectionResetError:
+            break
+        except socket.timeout:
+            break
+
+    # write data received into a local file
+    filename = '/home/yijie/Desktop/receive.txt'
+    with open(filename, 'w') as f:
+        for data in receive_data:
+            f.write(data)
+    socket.close()
+    return
+
+
+if __name__ == '__main__':
+    receiver()
---
+++ sender.py
@@ -0,0 +1,144 @@
+import socket
+import struct
+import binascii
+import threading
+import time
+
+
+# alice 10.3.81.2 may change
+
+r_next = 0  # next sequence number to send
+size_window = 8  # size of sending window
+max_seq = 16  # maximum of sequence number
+buffer = {}  # buffer to save packets
+send_channel_lock = threading.lock()
+r_next_lock = threading.lock()
+curr_seq = 0
+
+
+def parse_pkt(pkt):
+    """
+    Parse the received packet
+    Args:
+        pkt: packet received
+    Returns:
+        seq_num: sequence number
+        crc_num: crc number
+        data: data contained in the packet
+    """
+    seq_num = struct.unpack('=I', pkt[0:4])[0]
+    crc_num = struct.unpack('=H', pkt[4:6])[0]
+    data = pkt[6:].decode('UTF-8', errors='ignore')
+    return seq_num, crc_num, data
+
+
+class Ack(threading.Thread):
+    def __init__(self, socket):
+        threading.Thread.__init__(self)
+        self.socket = socket
+        self.start()
+
+    def run(self):
+        while True:
+            if getattr(self.socket, '_closed'):
+                return
+            ack_pkt = self.socket.recv(2048)
+            seq, ack, next = self.parse(ack_pkt)
+            # Lock r_next so that it can not be read when changing
+            r_next_lock.acquire()
+            prev_r_next = r_next
+            r_next = next
+            r_next_lock.release()
+
+            if ack != 0:
+                send_channel_lock.acquire()
+                if seq in buffer.keys():
+                    del buffer[seq]
+                    for i in range((r_next - prev_r_next) % max_seq):
+                        if (prev_r_next + i) % max_seq in buffer.keys():
+                            del buffer[(prev_r_next + i) % max_seq]
+                send_channel_lock.release()
+            else:
+                send_channel_lock.acquire()
+                buffer[next] = (buffer[next][0], time.time(), 0)
+                del buffer[seq]
+                self.socket.send(buffer[next][0])
+                send_channel_lock.release()
+
+
+class Sending(threading.Thread):
+    def __init__(self, socket):
+        threading.Thread.__init__(self)
+        self.socket = socket
+        self.start()
+
+    def run(self):
+        # TODO:
+        f = open('/home/yijie/Desktop/receive.txt', 'r')
+        first_send = True
+        eof = False
+
+        while True:
+            r_next_lock.acquire()
+            temp_rnext = r_next
+            r_next_lock.release()
+            if len(buffer) == 0 and eof:
+                break
+            send_channel_lock.acquire()
+            for seq in buffer.keys():
+                if buffer[seq][2] == 0 and time.time() - buffer[seq][1] > 3.6:
+                    buffer[seq] = (buffer[seq][0], time.time(), 0)
+                    self.socket.send(buffer[seq][0])
+            send_channel_lock.release()
+            if not eof:
+                if first_send:
+                    for i in range(size_window):
+                        send_channel_lock.acquire()
+                        seq_num = (curr_seq + i) % max_seq
+                        if seq_num not in buffer.keys():
+                            data = f.read(1024)
+                            if not data:
+                                eof = True
+                                send_channel_lock.release()
+                                break
+                            else:
+                                packet = struct.pack('=I', seq_num) + struct.pack(
+                                    '=H', binascii.crc_hqx(bytes(data, 'UTF-8'), 0)) + bytes(data, 'UTF-8')
+                                buffer[seq_num] = (packet, time.time(), 0)
+                                self.socket.send(packet)
+                        send_channel_lock.release()
+                        time.sleep(0.4)
+                    first_send = False
+                else:
+                    for i in range(0, (temp_rnext - curr_seq) % max_seq):
+                        send_channel_lock.acquire()
+                        seq_num = (curr_seq + i + size_window) % max_seq
+                        if seq_num not in buffer.keys():
+                            data = f.read(1024)
+                            if not data:
+                                eof = True
+                                send_channel_lock.release()
+                                break
+                            else:
+                                packet = struct.pack('=I', seq_num) + struct.pack(
+                                    '=H', binascii.crc_hqx(bytes(data, 'UTF-8'), 0)) + bytes(data, 'UTF-8')
+                                buffer[seq_num] = (packet, time.time(), 0)
+                                self.socket.send(packet)
+                        send_channel_lock.release()
+                        time.sleep(0.4)
+
+            curr_seq = temp_rnext
+        self.socket.close()
+        f.close()
+
+
+if __name__ == '__main__':
+    send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # TODO add rec IP address
+    rec_eth = ""
+    send_socket.connect(("", 10000))
+
+    sending_thread = Sending(send_socket)
+    ack_thread = Ack(send_socket)
+    sending_thread.join()
+    ack_thread.join()
